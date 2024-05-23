bf084f567d441044860b54951379f2b36e9566a5
caiohenrqq <caiohenrqq@gmail.com>
2024-05-23 17:56:03 -0400

    Alimenta Sessão Suporte

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -6,9 +6,9 @@ const Support = () => {
     <div className='support container section'>
       <div className='sectionContainer'>
         <div className="titlesDiv">
-          <small>Suporte de Viagem</small>
-          <h2>Plan your travel with confidence</h2>
-          <p>Find help with booking and travel plans, see what to expect along</p>
+          <small>Suporte de Viagens</small>
+          <h2>Planeje sua viagem com confiância!</h2>
+          <p>Encontre ajuda para reservas e planos de viagem e veja o que esperar ao longo do caminho.</p>
         </div>
 
         <div className="infoDiv">
@@ -16,25 +16,25 @@ const Support = () => {
 
             <div className="singleInfo">
               <span className="number">01</span>
-              <h4>Travel requirements for Dubai</h4>
+              <h4>Requisitos de Viagem</h4>
               <p>
-                Find help with booking and travel plans, see what to expect along the journey to your favorite destinations!
+                Encontre ajuda para reservas e planos de viagem, e saiba o que esperar ao longo da jornada para os seus destinos favoritos! 
               </p>
             </div>
 
             <div className="singleInfo">
               <span className="number">02</span>
-              <h4>Chauffeur services at your arrival</h4>
+              <h4>Serviço de Motorista à sua Chegada</h4>
               <p>
-                Find help with booking and travel plans, see what to expect along the journey to your favorite destinations!
+                Relaxe! Motorista profissional te espera no aeroporto, ajuda com bagagem e te leva ao destino com conforto e segurança.
               </p>
             </div>
 
             <div className="singleInfo">
               <span className="number">03</span>
-              <h4>Multi-risk travel insurance </h4>
+              <h4>Seguro de Viagem Multirrisco</h4>
               <p>
-                Find help with booking and travel plans, see what to expect along the journey to your favorite destinations!
+                Viaje tranquilo com nosso Seguro de Viagem Multirrisco, cobrindo saúde, bagagem, voos e emergências, garantindo proteção completa.
               </p>
             </div>
 
